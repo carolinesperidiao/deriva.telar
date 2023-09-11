701df7723f37d4879ffa2e995b0bdcc16c231947
carolinesperidiao <124894829+carolinesperidiao@users.noreply.github.com>
2023-09-11 18:44:17 -0300

    Upload dos pedaços de codigo

---
+++ Pedacos de codigo/nick/snippet_header.php
@@ -0,0 +1,25 @@
+<div class="wrap-menus" id="wrap-clickout">            
+        <div class="startmenu" id="menu-topicos">
+        <div class="titulo-topicos"><h3>Tópicos</h3> <svg onclick="benchtest()" id="start-fechar" class="ico-fechar" width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9 6H6V9H9V6Z" fill="black"/>
+<path d="M11.9997 9.0002L9 9L8.99973 12.0002H11.9997V9.0002Z" fill="black"/>
+<path d="M11.9997 12.0002L15.0004 11.9999V14.9999H12.0004L11.9997 12.0002Z" fill="black"/>
+<path d="M17.9996 15.0001L15.0004 14.9999L15.0001 17.9999H12.0004V20.9999H15.0004L15.0001 17.9999L17.9996 18.0001V21.0001H20.9996V18.0001L17.9996 18.0001V15.0001H20.9996V12.0001L17.9996 12.0001V15.0001Z" fill="black"/>
+<path d="M12.0004 20.9999L8.99973 21.0001V23.9998H6V26.9998H9L8.99973 23.9998L11.9997 24.0001L12.0004 20.9999Z" fill="black"/>
+<path d="M27 27H24V24H27V27Z" fill="black"/>
+<path d="M24 24L21.0003 23.9998L20.9996 21.0001L24.0003 20.9998L24 24Z" fill="black"/>
+<path d="M20.9996 12.0001L24.0003 11.9999V9.0002L27 9.0002V6.0002L24 6.0002L24.0003 9.0002L21.0003 8.99993L20.9996 12.0001Z" fill="black"/>
+</svg>
+</div>
+        <div class="start-topicos">
+<nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+	<div id="search"><?php get_search_form(); ?></div>
+<?php bellows( 'main' ); ?>
+
+</nav>
+
+</div>
+</div>
+
+<div class="bar-nav"><div class="item-nav"><img class="icone-sixcolor" src="http://localhost:8888/tccdoc/wp-content/uploads/2023/09/Untitled-8.gif">Home</div> <div class="item-nav" id="icone-topicos"><img class="icone-sixcolor" src="http://localhost:8888/tccdoc/wp-content/uploads/2023/09/Untitled-8.gif">Tópicos</div><div class="item-nav" id="icone-sobre"><img class="icone-sixcolor" src="http://localhost:8888/tccdoc/wp-content/uploads/2023/09/Untitled-8.gif">Sobre</div><div onclick="togglemodo()" class="item-nav"><img class="icone-sixcolor" src="http://localhost:8888/tccdoc/wp-content/uploads/2023/09/Untitled-8.gif">Tema claro</div></div>
+</div>
